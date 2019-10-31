6fd8446195530ff3243d6ced999605bc87245388
TamuraYu <tamura.yu@image.iit.tsukuba.ac.jp>
2019-10-31 14:39:54 +0900

    first commit で上がらなかった残り?

---
+++ Assets/script/flow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flow : MonoBehaviour
+{
+
+    private float time;
+    public int offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f, 0f, Time.deltaTime);
+        time += Time.deltaTime;
+        if(time > 30f)
+        {
+            time -= 30f;
+            transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 30f);
+        }
+    }
+}
